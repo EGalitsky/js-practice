2a4122f4ef10b355593579f95fc641ad9a6c96dd
Evgeny Galitsky <e.galitsky@bk.ru>
2022-07-02 22:16:45 +0300

    Fix some bad code

--- starter/Modern-JS-Modules-Tooling/script.js
+++ starter/Modern-JS-Modules-Tooling/script.js
@@ -69,3 +69,11 @@ if (module.hot) {
 }
 
 console.log(cart.find(el => el.quantity >= 2));
+Promise.resolve('TEST').then(x => console.log(x));
+
+import 'core-js/stable';
+// import 'core-js/stable/array/find';
+// import 'core-js/stable/promise';
+
+// Polifilling async functions
+import 'regenerator-runtime/runtime';
